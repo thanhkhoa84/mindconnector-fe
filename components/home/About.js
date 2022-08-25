fa69a8305c343588d3c3ec8996e9a2b11dc888cb
Khoa Nguyen <thanhkhoa84@gmail.com>
2022-08-25 14:04:45 +0700

    homepage - about section

--- components/home/About.js
+++ components/home/About.js
@@ -1,9 +1,23 @@
+import YouTube from 'react-youtube';
+
 import Container from '../../components/Container';
 import { SectionHeading } from '../../components/Heading';
 import styles from './About.module.scss';
 import LogoList from './../LogoList';
 
 const About = () => {
+  const opts = {
+    height: 'auto',
+    width: '100%',
+    playerVars: {
+      autoplay: 0,
+    },
+  };
+
+  const _onReady = (event) => {
+    event.target.pauseVideo();
+  };
+
   return (
     <section className={styles.AboutMi}>
       <Container>
@@ -12,6 +26,9 @@ const About = () => {
           Mind Connector kết nối tầm nhìn và tri thức để giúp doanh nghiệp, cá
           nhân phát triển và tăng trưởng mạnh mẽ trong tương lai.
         </p>
+        <div className={styles.videoEmbed}>
+          <YouTube videoId='8dJyRm2jJ-U' opts={opts} onReady={_onReady} />
+        </div>
       </Container>
       <LogoList
         title='Đối tác'
