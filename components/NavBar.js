276d4e37421327d7d3d534535684d23773851de5
Khoa Nguyen <thanhkhoa84@gmail.com>
2022-09-03 22:14:36 +0700

    udapte navbar

--- components/NavBar.js
+++ components/NavBar.js
@@ -2,13 +2,13 @@ import Link from 'next/link';
 import Image from 'next/image';
 import { useRouter } from 'next/router'
 
-import { forwardRef, useEffect, useState } from 'react';
+import { forwardRef, useEffect, useRef, useState } from 'react';
 
 import styles from '../styles/components/NavBar.module.scss';
 
-const DropdownMenu = ({ items, isOpen, dropdId }) => {
+const DropdownMenu = ({ items, dropdown, dropdId }) => {
   return (
-    <ul id={dropdId} className={`rounded-b-2xl shadow-xl m-0 flex flex-col absolute shadow-2xl bg-white p-4 lg:top-[55px] left-[-30px] lg:w-[320px] ${isOpen ? 'block' : 'hidden'}`}>
+    <ul id={dropdId} className={`rounded-b-2xl shadow-xl m-0 flex flex-col absolute shadow-2xl bg-white p-4 lg:top-full left-[-30px] lg:w-[320px] ${dropdown ? 'block' : 'hidden'}`}>
       {items.map((item, index) => {
         return (
           <li key={index} className='w-full font-normal leading-8'>
@@ -22,62 +22,87 @@ const DropdownMenu = ({ items, isOpen, dropdId }) => {
   );
 };
 
-const NavItems = forwardRef(({ navlinks }, ref) => {
-  const [isOpen, setIsOpen] = useState(false);
+const NavItem = ({link, index}) => {
+  const [dropdown, setDropdown] = useState(false);
   const router = useRouter();
+  let ref = useRef();
 
+  const handleRouteChange = (url, { shallow }) => {
+    setDropdown(false)
+  }
+  
   const onClickHandler = (e) => {
-    setIsOpen((isOpen) => !isOpen);
+    setDropdown((dropdown) => !dropdown);
     e.preventDefault();
   };
 
-  useEffect(() => {
-    if(isOpen) return;
-
-    const handleRouteChange = (url, { shallow }) => {
-      setIsOpen(false)
+  const handleClickOutside = (event) => {
+    if (ref.current && !ref.current.contains(event.target)) {
+      setDropdown(false);
     }
+  }
 
+  const onMouseEnter = () => {
+    window.innerWidth > 960 && setDropdown(true);
+  };
+
+  const onMouseLeave = () => {
+    window.innerWidth > 960 && setDropdown(false);
+  };
+
+  useEffect(() => {
+    document.addEventListener("mousedown", handleClickOutside);
     router.events.on("hashChangeStart", handleRouteChange);
     router.events.on('routeChangeStart', handleRouteChange)
 
     return () => {
       router.events.off('routeChangeStart', handleRouteChange)
       router.events.off('hashChangeStart', handleRouteChange)
+      document.removeEventListener("mousedown", handleClickOutside);
     }
-  }, [])
+  },[dropdown]);
 
+  return(
+    <li
+      className={`
+        flex
+        items-center
+        justiofy-center
+        h-[80px]
+        ${link.submenus ? 'group relative' : ''} 
+      `}
+      ref={ref}
+      key={index}
+      onMouseEnter={onMouseEnter}
+      onMouseLeave={onMouseLeave}
+    >      
+      {link.submenus ? 
+        (
+          <>
+            <button onClick={onClickHandler} data-dropdown-toggle={`dropdown-${index}`} aria-expanded={dropdown ? "true" : "false"} className='hover:color-purple' onClick={onClickHandler}>{link.name}</button>
+            <DropdownMenu dropdId={`dropdown-${index}`} items={link.submenus} dropdown={dropdown}/>
+          </>
+        )
+          :
+        (<Link href={link.path} className='hover:text-purple'>
+          {link.name}
+        </Link>)
+      }
+    </li>
+  )
+}
+
+const NavItems = ({ navlinks }) => {
   return (
     <ul className='m-0 p-0 z-10 relative h-[100%] font-bold justify-center items-center lg:flex lg:space-x-6 items-center lg:flex-row lg:mr-6'>
       {navlinks.map((link, index) => {
         return (
-          <li
-            className={`
-              items-center
-              justiofy-center
-              ${link.submenus ? 'group relative' : ''} 
-            `}
-            key={index}
-          >
-            {link.submenus ? 
-              (
-                <>
-                  <button onClick={onClickHandler} data-dropdown-toggle={`dropdown-${index}`} className='hover:color-purple'>{link.name}</button>
-                  <DropdownMenu dropdId={`dropdown-${index}`} items={link.submenus} isOpen={isOpen}/>
-                </>
-              )
-                :
-              (<Link href={link.path} className='hover:text-purple'>
-                {link.name}
-              </Link>)
-            }
-          </li>
+          <NavItem link={link} index={index} key={index} />
         );
       })}
     </ul>
   );
-});
-NavItems.displayName = 'NavItems';
+};
 
 const NavBar = ({ ...navlinks }) => {
   return (
--- pages/_app.js
+++ pages/_app.js
@@ -50,37 +50,37 @@ MyApp.getInitialProps = async (ctx) => {
     {
       name: 'Dành cho doanh nghiệp',
       path: 'business',
-      // submenus: [
-      //   {
-      //     name: 'Thành Công Trong Học Tập',
-      //     path: '/student#thanh-cong-trong-hoc-tap',
-      //   },
-      //   {
-      //     name: 'Thành Công Trong Ứng Tuyển',
-      //     path: '/student#thanh-cong-trong-ung-tuyen',
-      //   },
-      //   {
-      //     name: 'Thành Công Trong Khởi Sự Kinh Doanh',
-      //     path: '/student#thanh-cong-trong-khoi-su-kinh-doanh',
-      //   },
-      //   {
-      //     name: 'Experience Tour',
-      //     path: '/student',
-      //   },
-      // ],
+      submenus: [
+        {
+          name: 'Thành Công Trong Học Tập',
+          path: '/student#thanh-cong-trong-hoc-tap',
+        },
+        {
+          name: 'Thành Công Trong Ứng Tuyển',
+          path: '/student#thanh-cong-trong-ung-tuyen',
+        },
+        {
+          name: 'Thành Công Trong Khởi Sự Kinh Doanh',
+          path: '/student#thanh-cong-trong-khoi-su-kinh-doanh',
+        },
+        {
+          name: 'Experience Tour',
+          path: '/student',
+        },
+      ],
     },
-    // {
-    //   name: 'Đối tác',
-    //   path: 'partners',
-    // },
-    // {
-    //   name: 'Tin tức & Sự kiện',
-    //   path: 'news',
-    // },
-    // {
-    //   name: 'Liên hệ',
-    //   path: 'contact',
-    // },
+    {
+      name: 'Đối tác',
+      path: 'partners',
+    },
+    {
+      name: 'Tin tức & Sự kiện',
+      path: 'news',
+    },
+    {
+      name: 'Liên hệ',
+      path: 'contact',
+    },
   ];
 
   // Calls page's `getInitialProps` and fills `appProps.pageProps`
