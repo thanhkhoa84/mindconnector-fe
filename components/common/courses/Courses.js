2eef1eefb6af4ca6a3eb585d5f8c5776ccdd9edd
Khoa Nguyen <thanhkhoa84@gmail.com>
2022-09-09 19:26:43 +0700

    fix grid layout for courses list

--- components/common/courses/Courses.js
+++ components/common/courses/Courses.js
@@ -9,14 +9,24 @@ const Courses = () => {
   return (
     <div
       className={`
-        text-justify my-[2em] mx-0 bg-transparent grid lg:grid-cols-5 md:grid-cols-4 sm:grid-cols-3 xs:grid-cols-2 grid-cols-1 gap-2 sm:gap-6 md:gap-5 content-start  
-        ${total <= 5 ? 'slider-less-than-five' : ''}`}
+        my-[2em] mx-0 grid grid-cols-1 content-start gap-2 bg-transparent text-justify xs:grid-cols-2 xs:gap-4 sm:grid-cols-3 sm:gap-6 md:grid-cols-4 md:gap-5 lg:grid-cols-5  
+        ${total <= 5 ? "slider-less-than-five" : ""}`}
     >
-      <CourseCard />
-      <CourseCard />
-      <CourseCard />
-      <CourseCard />
-      <CourseCard />
+      <div className="mx-auto xs:mx-0">
+        <CourseCard />
+      </div>
+      <div className="mx-auto xs:mx-0">
+        <CourseCard />
+      </div>
+      <div className="mx-auto xs:mx-0">
+        <CourseCard />
+      </div>
+      <div className="mx-auto xs:mx-0">
+        <CourseCard />
+      </div>
+      <div className="mx-auto xs:mx-0">
+        <CourseCard />
+      </div>
     </div>
   );
 };
--- tailwind.config.js
+++ tailwind.config.js
@@ -10,7 +10,7 @@ module.exports = {
   ],
   theme: {
     screens: {
-      xs: "475px",
+      xs: "375px",
       ...defaultTheme.screens,
     },
     extend: {
