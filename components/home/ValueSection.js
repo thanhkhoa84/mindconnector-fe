60af270891d24cc047fb82a295f747b89b886d0c
Khoa Nguyen <thanhkhoa84@gmail.com>
2022-09-12 10:05:00 +0700

    convert homepage value section to tailwind

--- components/home/ValueSection.js
+++ components/home/ValueSection.js
@@ -3,7 +3,6 @@ import dynamic from 'next/dynamic';
 import Image from "next/image";
 import Container from "../../components/Container";
 import { SectionHeading } from "../../components/Heading";
-import styles from "./Value.module.scss";
 
 const LogoList = dynamic(() => import("./../LogoList"), {
   ssr: false,
@@ -18,12 +17,12 @@ const ValueSection = () => {
           Cùng Mind Connector tạo ra những giá trị đích thực
         </SectionHeading>
         <div
-          className={`mt-[3em] mb-[4em] flex justify-between gap-6 self-stretch pb-[1em] lg:items-center lg:self-auto`}
+          className={`mt-[3em] mb-[4em] flex flex-col justify-between gap-6 self-stretch pb-[1em] xs:flex-row xs:gap-3 lg:items-center lg:gap-4 lg:self-auto`}
         >
-          <div className="relative flex flex-row">
+          <div className="relative flex flex-row  justify-between">
             <div
               className={`
-              relative my-[1.5em] ml-0 -mr-[2em] flex max-w-full flex-col rounded-2xl border-4 border-white bg-gradient-to-b from-[#f5cbcc] to-[#ffaf43] px-4 pt-[2.5em] pb-4 shadow-lg md:w-[55%] md:pr-[10%]
+              relative mt-[1.5em] ml-0 -mr-[2em] flex max-w-full flex-col justify-between rounded-2xl border-4 border-white bg-gradient-to-b from-[#f5cbcc] to-[#ffaf43] px-4 pt-[2.5em] pb-4 shadow-lg lg:my-[1.5em] lg:w-[55%] lg:pr-[2em]
             `}
             >
               <span className="absolute -top-[30px] block w-[66px]">
@@ -35,15 +34,15 @@ const ValueSection = () => {
                   alt=""
                 />
               </span>
-              <h2 className="text-xl font-black leading-6 md:text-[22px]">
+              <h2 className="text-[19px] font-black leading-6 md:text-[22px]">
                 Dành cho <br className="md:hidden" />
                 sinh viên
               </h2>
-              <p className="mt-4 hidden md:block">
+              <p className="mt-4 hidden lg:block">
                 Xây dựng nền móng vững chắc với các kỹ năng quan trọng để sẵn
                 sàng theo đuổi và thành công trong công việc hằng mong muốn.
               </p>
-              <p className="mt-4 md:hidden">
+              <p className="mt-4 lg:hidden">
                 Xây dựng nền móng vững chắc để sẵn sàng theo đuổi thành công.
               </p>
               <Link href="student">
@@ -52,7 +51,7 @@ const ValueSection = () => {
                 </span>
               </Link>
             </div>
-            <div className="relative z-10 hidden overflow-hidden rounded-3xl object-cover shadow-lg md:flex md:w-[300px]">
+            <div className="relative z-10 hidden overflow-hidden rounded-3xl object-cover shadow-lg lg:flex lg:w-[260px]">
               <img
                 src="/img/value-student.png"
                 alt=""
@@ -60,10 +59,10 @@ const ValueSection = () => {
               />
             </div>
           </div>
-          <div className="relative flex flex-row">
+          <div className="relative flex flex-row  justify-between">
             <div
               className={`
-              relative my-[1.5em] ml-0 -mr-[2em] flex max-w-full flex-col rounded-2xl border-4 border-white bg-gradient-to-b from-[#f5cbcc] to-[#ffaf43] px-4 pt-[2.5em] pb-4 shadow-lg md:w-[55%] md:pr-[10%]
+              relative mt-[1.5em] ml-0 -mr-[2em] flex max-w-full flex-col justify-between rounded-2xl border-4 border-white bg-gradient-to-b from-[#f5cbcc] to-[#ffaf43] px-4 pt-[2.5em] pb-4 shadow-lg lg:my-[1.5em] lg:w-[55%] lg:pr-[2em]
             `}
             >
               <span className="absolute -top-[30px] block w-[66px]">
@@ -75,15 +74,15 @@ const ValueSection = () => {
                   alt=""
                 />
               </span>
-              <h2 className="text-xl font-black leading-6 md:text-[22px]">
+              <h2 className="text-[19px] font-black leading-6 md:text-[22px]">
                 Dành cho <br className="md:hidden" />
                 doanh nghiệp
               </h2>
-              <p className="mt-4 hidden md:block">
+              <p className="mt-4 hidden lg:block">
                 Đào tạo năng lực của nhân viên để doanh nghiệp không chỉ có một
                 nguồn nhân lực chất lượng, mà còn có thể lớn mạnh hơn từng ngày.
               </p>
-              <p className="mt-4 md:hidden">
+              <p className="mt-4 lg:hidden">
                 Đào tạo nguồn nhân lực chất lượng để giúp công ty thêm lớn mạnh.
               </p>
               <Link href="business">
@@ -92,7 +91,7 @@ const ValueSection = () => {
                 </span>
               </Link>
             </div>
-            <div className="relative z-10 hidden overflow-hidden rounded-3xl object-cover shadow-lg md:flex md:w-[300px]">
+            <div className="relative z-10 hidden overflow-hidden rounded-3xl object-cover shadow-lg lg:flex lg:w-[260px]">
               <img
                 src="/img/value-business.png"
                 alt=""
