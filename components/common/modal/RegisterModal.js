1884a3348a9b13c57bd601a764ad8e3837be7c84
Khoa Nguyen <thanhkhoa84@gmail.com>
2022-10-09 06:49:21 +0700

    update

--- components/common/modal/RegisterModal.js
+++ components/common/modal/RegisterModal.js
@@ -55,7 +55,7 @@ const RegisterModal = () => {
       data,
     };
     let mailRes = await axios(mailConfig);
-    // let response = await axios(config);
+    let response = await axios(config);
     if (mailRes.status === 200) {
       hideModal();
     }
