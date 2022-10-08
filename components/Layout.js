0034eccc134f85be95d34767357b4de83f8cbd60
Khoa Nguyen <thanhkhoa84@gmail.com>
2022-10-08 23:34:08 +0700

    update

--- components/Layout.js
+++ components/Layout.js
@@ -11,7 +11,6 @@ export default function Layout({ children }) {
     <>
       <Head>
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-        <link rel="icon" type="image/png" href="/favicon.png" />
       </Head>
       <GlobalModal>
         <MenuProvider value={Navigation}>
