2fdd5f0683dacc2a860a72cd7a3579c5e272302d
Khoa Nguyen <thanhkhoa84@gmail.com>
2022-10-10 13:19:07 +0700

    update

--- next.config.js
+++ next.config.js
@@ -4,7 +4,7 @@ const nextConfig = {
   swcMinify: true,
   images: {
     domains: ["localhost:3000"],
-    unoptimized: false,
+    unoptimized: true,
   },
   exportPathMap: async function (
     defaultPathMap,
