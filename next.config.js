4c09b8b39f17e9c63dcf11d919e0640853670ff7
Khoa Nguyen <thanhkhoa84@gmail.com>
2022-10-15 12:03:15 +0700

    update

--- next.config.js
+++ next.config.js
@@ -3,8 +3,13 @@ const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
   images: {
-    domains: ["localhost:3000"],
+    domains: [
+      "localhost:3000",
+      "mindconnector.com.vn",
+      "www.mindconnector.com.vn",
+    ],
     unoptimized: false,
+    deviceSizes: [375, 640, 750, 992, 1140, 1920],
   },
   exportPathMap: async function (
     defaultPathMap,
