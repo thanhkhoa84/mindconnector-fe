304da04c7e3799e73b3d717236b259a2fe5eef3f
Khoa Nguyen <thanhkhoa84@gmail.com>
2022-10-15 11:14:46 +0700

    update

--- next.config.js
+++ next.config.js
@@ -5,7 +5,6 @@ const nextConfig = {
   images: {
     domains: ["localhost:3000"],
     unoptimized: false,
-    deviceSizes: [375, 640, 750, 828, 1080, 1200, 1920, 2048, 3840],
   },
   exportPathMap: async function (
     defaultPathMap,
