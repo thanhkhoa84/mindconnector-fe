efee8b7c8716787345bc3b4a56ea606fc0b82ef9
Khoa Nguyen <thanhkhoa84@gmail.com>
2022-11-11 08:19:33 +0700

    update services titles

--- pages/services/tu-van-quan-tri-su-thay-doi.js
+++ pages/services/tu-van-quan-tri-su-thay-doi.js
@@ -6,7 +6,7 @@ import Teachers from "@/components/common/teachers/Teachers";
 import LeadingForm from "@/components/LeadingForm";
 
 const seo = {
-  metaTitle: "Tư vấn chiến lược thương hiệu",
+  metaTitle: "Tư vấn quản trị sự thay đổi (Change Management)",
   metaDescription:
     "Mind Connector là một mạng lưới của các nhà tư vấn kinh doanh cùng các giảng viên vốn là các lãnh đạo và chuyên gia hàng đầu trong nhiều lĩnh vực khác nhau. Mind Connector kết nối tầm nhìn và tri thức để giúp doanh nghiệp, cá nhân phát triển và tăng trưởng mạnh mẽ trong tương lai",
 };
