f6ea77906670917f004b45596b562c6b88c64e3e
Khoa Nguyen <thanhkhoa84@gmail.com>
2023-07-15 22:09:57 +0700

    adding vercel config

--- next.config.js
+++ next.config.js
@@ -1,6 +1,27 @@
 /** @type {import('next').NextConfig} */
 
 const nextConfig = {
+  async headers() {
+    return [
+      {
+        // matching all API routes
+        source: "/api/:path*",
+        headers: [
+          { key: "Access-Control-Allow-Credentials", value: "true" },
+          { key: "Access-Control-Allow-Origin", value: "*" },
+          {
+            key: "Access-Control-Allow-Methods",
+            value: "GET,OPTIONS,PATCH,DELETE,POST,PUT",
+          },
+          {
+            key: "Access-Control-Allow-Headers",
+            value:
+              "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version",
+          },
+        ],
+      },
+    ];
+  },
   reactStrictMode: true,
   swcMinify: true,
   images: {
@@ -12,13 +33,18 @@ const nextConfig = {
         pathname: "/uploads/**",
       },
       {
-        protocol: "http",
+        protocol: "https",
         hostname: "be.mindconnector.vn",
         port: "",
         pathname: "/uploads/**",
       },
     ],
-    domains: ["localhost", "mindconnector.com.vn", "www.mindconnector.com.vn"],
+    domains: [
+      "localhost",
+      "mindconnector.com.vn",
+      "www.mindconnector.com.vn",
+      "be.mindconnector.vn",
+    ],
     unoptimized: false,
     deviceSizes: [375, 640, 750, 992, 1140, 1920],
   },
